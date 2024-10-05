fa097a334acc6ce8f16c024b944b965b59bc4402
depotcf <138543970+depotcf@users.noreply.github.com>
2024-10-06 07:42:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '3ebeac6e-1aad-409a-80f2-4fd11eb93c2f';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
